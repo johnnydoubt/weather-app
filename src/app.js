578a437402f41c9e061def28be45615ef7e5e9ca
OXATIS\cesar.tailleur <cesar.tailleur@oxatis.com>
2020-01-24 16:48:04 +0100

    added icons, big css update

--- src/app.js
+++ src/app.js
@@ -1,108 +1,109 @@
-const path = require('path')
-const express = require('express')
-const hbs = require('hbs')
+const path = require('path');
+const express = require('express');
+const hbs = require('hbs');
 
-const geocode = require('./utils/geocode')
-const forecast = require('./utils/forecast')
+const geocode = require('./utils/geocode');
+const forecast = require('./utils/forecast');
 
-const app = express()
-const port = process.env.PORT || 3000
+const app = express();
+const port = process.env.PORT || 3000;
 
 // Define paths for Express config
-const publicDirectoryPath = path.join(__dirname, '../public')
-const viewsPath = path.join(__dirname, '../templates/views')
-const partialsPath = path.join(__dirname, '../templates/partials')
+const publicDirectoryPath = path.join(__dirname, '../public');
+const viewsPath = path.join(__dirname, '../templates/views');
+const partialsPath = path.join(__dirname, '../templates/partials');
 
 // Setup handlebars engine and views location
-app.set('view engine', 'hbs')
-app.set('views', viewsPath)
-hbs.registerPartials(partialsPath)
+app.set('view engine', 'hbs');
+app.set('views', viewsPath);
+hbs.registerPartials(partialsPath);
 
 // Setup static dir to serve
-app.use(express.static(publicDirectoryPath))
+app.use(express.static(publicDirectoryPath));
 
 app.get('', (req, res) => {
-    res.render('index', {
-        title: 'Weather App',
-        name: 'Cesar Tailleur'
-    })
-})
+	res.render('index', {
+		title: 'Weather App',
+		name: 'Cesar Tailleur',
+		active: { home: true }
+	});
+});
 
 app.get('/about', (req, res) => {
-    res.render('about', {
-        title: 'About us',
-        name: 'Cesar Tailleur'
-    })
-})
+	res.render('about', {
+		title: 'About us',
+		name: 'Cesar Tailleur',
+		active: { about: true }
+	});
+});
 
 app.get('/help', (req, res) => {
-    res.render('help', {
-        title: 'Help',
-        helpText: 'This is where you get help when you need some.',
-        name: 'Cesar Tailleur'
-    })
-})
+	res.render('help', {
+		title: 'Help',
+		helpText: 'This is where you get help when you need some.',
+		name: 'Cesar Tailleur'
+	});
+});
 
 app.get('/weather', (req, res) => {
-    if (!req.query.address) {
-        return res.send({
-            error: 'You must provide an address!'
-        })
-    }
-    geocode(req.query.address, (error, {
-        latitude,
-        longitude,
-        location
-    } = {}) => {
+	if (!req.query.address) {
+		return res.send({
+			error: 'You must provide an address!'
+		});
+	}
+	geocode(
+		req.query.address,
+		(error, { latitude, longitude, location } = {}) => {
+			if (error) {
+				res.send({
+					error
+				});
+			}
 
-        if (error) {
-            res.send({
-                error
-            })
-        }
+			forecast(latitude, longitude, (error, forecastData) => {
+				if (error) {
+					res.send({
+						error
+					});
+				}
 
-        forecast(latitude, longitude, (error, forecastData) => {
-            if (error) {
-                res.send({
-                    error
-                })
-            }
-
-            res.send({
-                forecast: forecastData,
-                location,
-                address: req.query.address
-            })
-        })
-    })
-})
+				res.send({
+					forecast: forecastData.info,
+					icon: forecastData.icon,
+					location,
+					address: req.query.address
+				});
+			});
+		}
+	);
+});
 app.get('/products', (req, res) => {
-    if (!req.query.search) {
-        return res.send({
-            error: 'You must provide a search term'
-        })
-    }
-    res.send({
-        products: []
-    })
-})
+	if (!req.query.search) {
+		return res.send({
+			error: 'You must provide a search term'
+		});
+	}
+	res.send({
+		products: []
+	});
+});
 
 // Mettre le 404 à la fin pour qu'il poppe uniquement si no match
 app.get('/help/*', (req, res) => {
-    res.render('404', {
-        title: '404',
-        errorText: 'Help article not found',
-        name: 'Cesar Tailleur'
-    })
-})
+	res.render('404', {
+		title: '404',
+		errorText: 'Help article not found',
+		name: 'Cesar Tailleur'
+	});
+});
 app.get('*', (req, res) => {
-    res.render('404', {
-        title: '404',
-        errorText: 'Page not found',
-        name: 'Cesar Tailleur'
-    })
-})
+	res.render('404', {
+		title: '404',
+		errorText: 'Page not found',
+		name: 'Cesar Tailleur'
+	});
+});
 
 app.listen(port, () => {
-    console.log('Server is up on port ' + port)
-})
+	console.log('Server is up on port ' + port);
+});
--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,31 +1,36 @@
-const request = require('request')
+const request = require('request');
 
-const forecast = (
-    latitude,
-    longitude,
-    callback) => {
-    const url = 'https://api.darksky.net/forecast/48a6ae47983660d76e532b46475c7489/' + latitude + ',' + longitude + '?lang=fr&units=si'
+const forecast = (latitude, longitude, callback) => {
+	const url =
+		'https://api.darksky.net/forecast/48a6ae47983660d76e532b46475c7489/' +
+		latitude +
+		',' +
+		longitude +
+		'?lang=fr&units=si';
 
-    request({
-        url,
-        json: true
-    }, (error, {
-        body
-    }) => {
-        if (error) {
-            callback('Unable to connect to weather services!', undefined)
-        } else if (body.error) {
-            callback('Unable to find location.', undefined)
-        } else {
-            callback(undefined,
-                `${body.daily.data[0].summary} 
-                Il fait actuellement ${body.currently.temperature} degrés. \n
-                Il y a ${body.currently.precipProbability}% de chances de pluie. \n
-                Température maximale de la journée: ${body.daily.data[0].temperatureHigh}. \n
-                Température minimale de la journée: ${body.daily.data[0].temperatureLow}. \n
-                `)
-        }
-    })
-}
+	request(
+		{
+			url,
+			json: true
+		},
+		(error, { body }) => {
+			if (error) {
+				callback('Unable to connect to weather services!', undefined);
+			} else if (body.error) {
+				callback('Unable to find location.', undefined);
+			} else {
+				callback(undefined, {
+					info: `${body.daily.data[0].summary} 
+                    Il fait actuellement ${body.currently.temperature} degrés. \n
+                    Il y a ${body.currently.precipProbability}% de chances de pluie. \n
+                    Température maximale de la journée: ${body.daily.data[0].temperatureHigh}. \n
+                    Température minimale de la journée: ${body.daily.data[0].temperatureLow}. \n
+                    `,
+					icon: body.currently.icon
+				});
+			}
+		}
+	);
+};
 
-module.exports = forecast
+module.exports = forecast;
